2b6620fd30711b1046b63b4f35720124e9a9239c
Jorge Venegas <veljorge@live.com>
2022-07-11 11:51:40 -0500

    Post Card template working

---
+++ network/static/network/index.js
@@ -0,0 +1,9 @@
+import Post from "./posts.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#posts-view").appendChild(Post({ title: "Title", content: "content", timestamp: "time", liked: true, likes: 10, comments: 7, id: 1, author: "Author" }))
+    for (let i = 0; i < 100; i++) {
+        document.querySelector("#posts-view").appendChild(Post({ title: "Title", content: "content", timestamp: "time", liked: false, likes: 10, comments: 5, id: 1, author: "Author" }))
+    }
+})
+
